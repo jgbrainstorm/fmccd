e4aefa4cb9e9427405b2d09ea1e99cadc80a2118
jiangang.steven.hao@gmail.com <jiangang.steven.hao@gmail.com@9c77bf7a-f8c1-7491-db5b-7b66a9f8ec79>
2010-12-30 18:04:18 +0000

    centroid change updated

--- trunk/centroid_change.py
+++ trunk/centroid_change.py
@@ -21,10 +21,12 @@ if len(sys.argv) == 1:
     print 'syntax: centroid_pos imageName1 imageName2'
 else:
     ext=[2,19,20,21,37,38,43,44,59,60]
+    print '------'+sys.argv[1]+' vs. '+sys.argv[2]+'----'
+    print 'CCD Position------dot change in pixels'
     for i in ext:
         data1,hdr1=pf.getdata(sys.argv[1],i,header=True)
-        x1,y1=getxy(data1,sys.argv[1]+'['+sys.argv[3]+']')
+        x1,y1=getxy(data1)
         data2,hdr2=pf.getdata(sys.argv[2],i,header=True)
-        x2,y2=getxy(data2,sys.argv[2]+'['+sys.argv[3]+']')
+        x2,y2=getxy(data2)
         sep=np.sqrt((x1-x2)**2+(y1-y2)**2)
-        print hdr1['detpos'],'--x1--',x1,'--y1--',y1,'--x2--',x2,'--y2--',y2,'--sep--',sep
+        print hdr1['detpos']+': '+str(np.round(sep,3))
